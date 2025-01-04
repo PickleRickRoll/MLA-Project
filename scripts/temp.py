09d95eed39bebbac9c477d581388dd3ae25e15ec
Ali <alighoul7654@gmail.com>
2025-01-04 04:22:59 +0100

    fixed bugs

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,50 +1,49 @@
+from utils import cqt , harmonic_stack , dsp 
 import numpy as np
-import matplotlib.pyplot as plt
+from modelv1 import model_v1
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import ReLU
+import tensorflow.keras.layers as nn
+from tensorflow.keras.layers import concatenate
 
-import librosa
-import librosa.display
-
-
-
+path='C:/Users/admin/Desktop/master2/MLA/Datasets/vocadito/audio/vocadito_9.wav'#Datasets/MTG-QBH/audio projet/C_major_scale.wav
 sample_rate=44100
 f_min=32.7
-n_harmonics=7
-harmonics=[0.5,1,2,3,4,5,6]
+n_harmonics=8
+harmonics=[0.5,1,2,3,4,5,6,7]
 hop_length=512
-bins_per_semitone=1
+bins_per_semitone=3
 bins_per_octave=12*bins_per_semitone
 n_bins=bins_per_octave*n_harmonics
-output_freq=500
-path='C:/Users/admin/Desktop/master2/MLA/projet/C_major.wav'
-
-y, sr = librosa.load(path, sr=None)
-print(y.shape,sr)
-
-shifts = np.round(12.0 * bins_per_semitone * np.log2(harmonics)).astype(int)# Calculate shifts for each harmonic
-print(shifts)
+output_freq=500#pas utiliser pour le momment 
 
 
-# Example 2D array (representing a small spectrogram or CQT result)
-test1 = np.array([[1, 2, 3],
-                        [4, 5, 6],
-                        [7, 8, 9]])
-print("Original Array:")
-print(test1.shape)
-print(test1)
-padded_right = np.pad(test1, pad_width=((1, 0), (0, 2)), mode='constant')
-print("\nPadded on the Right:")
-print(padded_right)
-padded_left = np.pad(test1, pad_width=((0, 0), (1, 0)), mode='constant')
-print("\nPadded on the Left:")
-print(padded_left)
 
-# Example 2D arrays
-array1_2d = np.array([[1, 2], [3, 4]])
-array2_2d = np.array([[5, 6], [7, 8]])
-print('shape array 1 =',array1_2d.shape)
-concatenated_2d_axis0 = np.concatenate((array1_2d, array2_2d), axis=0)
-print("\nConcatenated 2D Arrays along axis 0:\n", concatenated_2d_axis0)
-concatenated_2d_axis1 = np.concatenate((array1_2d, array2_2d), axis=-1)
-print("\nConcatenated 2D Arrays along axis 1:\n", concatenated_2d_axis1)
-print('shape concat 1 =',concatenated_2d_axis1.shape)
-#concat selon un axe , ex selon les colonnes je met ligne 1 arr 1 en serie  ligne 1 arr 2 , met les 2 mat en serie
+signal,sr=dsp(path)
+cqt_result=cqt(signal,sr,hop_length,f_min,n_bins,bins_per_octave,plot=False)
+print(cqt_result.shape)  # Should give (n_times, n_freqs)
+
+result=harmonic_stack(cqt_result, sr, harmonics, hop_length, bins_per_semitone,output_freq,plot=False)
+print(result.shape)
+
+model=model_v1(result.shape)
+model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        loss={
+            'onset': 'binary_crossentropy',
+            'note': 'binary_crossentropy',
+            'multipitch': 'binary_crossentropy',
+        },
+        metrics={
+            'onset': 'accuracy',
+            'note': 'accuracy',
+            'multipitch': 'accuracy',
+        },
+        loss_weights={'onset': 0.95, 'note': 1.0, 'multipitch': 1.0}
+    )
+
+input=np.expand_dims(result, axis=0)
+output=model.predict(input)
+print("Model Output Shape:", output)
